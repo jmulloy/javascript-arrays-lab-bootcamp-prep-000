8cb0eacc65d7513af66587bc8893ab965474d9c8
Josh Mulloy <joshmulloy1@gmail.com>
2018-05-17 18:35:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyRemoveFirstKitten(name) {
 }
 function appendKitten (name) {
   var newKittens = [...array]
-  newKittens.push=[...array]
+  newKittens.push(name)
   return newKittens
 }
