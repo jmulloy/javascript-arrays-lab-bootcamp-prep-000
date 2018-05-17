11ecb582f7073b60c3388c00695839288d510775
Josh Mulloy <joshmulloy1@gmail.com>
2018-05-17 18:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much."
+var kittens = ["milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+function appendKitten (name, broom) {
+  return newArray(broom, ...name)
+}
